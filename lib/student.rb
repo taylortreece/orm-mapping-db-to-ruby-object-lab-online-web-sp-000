40213c894b148b92d43899bb02114aab05300b13
Taylor Treece <taylor.treece@protonmail.com>
2021-02-18 10:47:09 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,89 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  @@all = []
+
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+
+    DB[:conn].execute(sql).each do |row|
+      @@all<<self.new_from_db(row)
+    end
+    @@all
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    a=[]
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, num).each do |row|
+      a<<self.new_from_db(row)
+    end
+    a
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10 LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).each do |row|
+      @a = self.new_from_db(row)
+    end
+    @a
+  end
+
+  def self.all_students_in_grade_X(grade)
+    a = []
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).each do |row|
+      a<<self.new_from_db(row)
+    end
+    a
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade <= 11
+    SQL
+    a = []
+    DB[:conn].execute(sql).each do |row|
+      a << self.new_from_db(row)
+    end
+    a
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row| 
+      self.new_from_db(row)
+    end.first
   end
   
   def save
